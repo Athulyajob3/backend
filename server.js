e814c31d8ca0f67dcf23e74b8c74eefe6df3b68b
Athul <athulyajob3@gmail.com>
2025-08-31 22:41:36 +0530

    Added CORS and updated start script

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // server.js
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 5000;
-
+server.use(cors()); // Enable CORS for all origins
 server.use(middlewares);
 server.use(router);
 
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`🚀 JSON Server is running on port ${PORT}`);
 });
